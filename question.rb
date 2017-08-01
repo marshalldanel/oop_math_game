bd5748ce313b9dc2c56f9019b56aaaf8b10dc320
marshalldanel <marshalldanel@gmail.com>
2017-08-01 18:43:34 +0000

    Commit initial working game

---
+++ question.rb
@@ -0,0 +1,32 @@
+
+class Question
+
+  attr_reader :answer
+
+  def initialize
+    @first_num = rand(1..20)
+    @second_num = rand(1..20)
+    @question = rand(1..4)
+
+    case @question
+    when 1
+      @answer = @first_num - @second_num
+      @operator = '-'
+    when 2
+      @answer = @first_num + @second_num
+      @operator = '+'
+    else
+      @answer = @first_num * @second_num
+      @operator = '*'
+    end
+  end
+
+  def to_s
+    "What is #{@first_num} #{@operator} #{@second_num}?"
+  end
+  
+  def right_answer?(user_input)
+    user_input == @answer
+  end
+
+end
